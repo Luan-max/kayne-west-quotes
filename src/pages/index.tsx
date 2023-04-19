145ee2eeb9401bb0271db809f856f7685ccb7f5a
Luan-max <luannlopessiva@outlook.com>
2023-04-18 21:42:00 -0300

    feat: add spotify iframe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,23 @@ interface HomeProps {
 }
 
 export default function Home({ data }: HomeProps) {
-  console.log(data);
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-black">
-      <span className="flex flex-col items-center justify-center text-3xl font-medium animate-bounce text-white lg:text-4xl p-3">
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-neutral-950 via-stone-900 to-neutral-950">
+      <span className="flex flex-col items-center justify-center text-2xl 2xl:text-3xl font-medium animate-bounce text-white lg:text-4xl p-3">
         {data.quote}
       </span>
-      <span className="relative top-4 text-sm font-medium text-white">- Kayne West</span>
+      <span className="relative top-4 text-sm font-medium text-white">
+        - Kayne West
+      </span>
+      <div className="absolute top-4 right-5 sm:right-16 md:right-32 lg:right-6 xl:right-8 2xl:right-3">
+        <iframe
+          className="rounded-xl w-80 sm:w-99 lg:w-99 xl:w-99 2xl:w-99 h-40"
+          src="https://open.spotify.com/embed/album/5CnpZV3q5BcESefcB3WJmz?utm_source=generator&theme=0"
+          allowFullScreen={true}
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
     </div>
   );
 }
